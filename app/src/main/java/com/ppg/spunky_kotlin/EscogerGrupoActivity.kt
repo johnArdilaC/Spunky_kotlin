d1e68d407c4fd367b73637318e661e32cbafa904
Maria Arevalo <mi.arevalo10@uniandes.edu.co>
2018-03-15 19:33:32 -0500

    dos actividades working

---
+++ app/src/main/java/com/ppg/spunky_kotlin/EscogerGrupoActivity.kt
@@ -0,0 +1,13 @@
+package com.ppg.spunky_kotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class EscogerGrupoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.AppThemeBar)
+        setContentView(R.layout.activity_escoger_grupo)
+    }
+}
--- app/src/main/java/com/ppg/spunky_kotlin/MainActivity.kt
+++ app/src/main/java/com/ppg/spunky_kotlin/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.ppg.spunky_kotlin
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener {
+            val intent = Intent(this, EscogerGrupoActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
