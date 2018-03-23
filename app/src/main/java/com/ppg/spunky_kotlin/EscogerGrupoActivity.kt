bad69a60f26cb53d4170d3bfee8e1b668d6843e2
Maria Arevalo <mi.arevalo10@uniandes.edu.co>
2018-03-23 11:37:08 -0500

    starting reading firebase

--- app/src/main/java/com/ppg/spunky_kotlin/EscogerGrupoActivity.kt
+++ app/src/main/java/com/ppg/spunky_kotlin/EscogerGrupoActivity.kt
@@ -2,11 +2,44 @@ package com.ppg.spunky_kotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.*
 
 class EscogerGrupoActivity : AppCompatActivity() {
+
+    // Write a message to the database
+    private val grupos: MutableList<String> = mutableListOf()
+
+    private val mRootDB: FirebaseDatabase = FirebaseDatabase.getInstance()
+    private val gruposReference: DatabaseReference = mRootDB.reference.child("Grupos")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_escoger_grupo)
+        initGrupos()
+        Log.d("Da reference", gruposReference.toString())
+
+        Log.d("Da groups", grupos.toString())
+
+    }
+
+    //Get stuff from DB
+    private fun initGrupos() {
+        val gruposListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                Log.d("Da groups", dataSnapshot.toString())
+
+                dataSnapshot.children.forEach {
+                    grupos.add(it.value.toString())}
+                Log.d("Da groups", grupos.toString())
+
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                println("loadPost:onCancelled ${databaseError.toException()}")
+            }
+        }
+
+        gruposReference.addListenerForSingleValueEvent(gruposListener)
     }
 }
